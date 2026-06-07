65a978a2a9a968ab167070a680be2ee5f89bc892
Maxime VANPEENE <190754696+maxvanp@users.noreply.github.com>
2026-06-07 10:07:38 +0200

    Cover alert trigger resource helpers

---
+++ internal/resources/alerttrigger/alert_trigger_resource_internal_test.go
@@ -0,0 +1,123 @@
+package alerttrigger
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+func TestValidateTriggerTypeNormalizesAcceptedValues(t *testing.T) {
+	for _, value := range []string{"too_many_login_failures", "TOO_MANY_LOGIN_FAILURES", "risk_assessment", "RISK_ASSESSMENT"} {
+		if err := validateTriggerType(value); err != nil {
+			t.Fatalf("validateTriggerType(%q) returned error: %v", value, err)
+		}
+	}
+}
+
+func TestValidateTriggerTypeRejectsUnknownValue(t *testing.T) {
+	if err := validateTriggerType("unknown"); err == nil {
+		t.Fatal("validateTriggerType(unknown) returned nil, want error")
+	}
+}
+
+func TestFindTriggerMatchesNormalizedType(t *testing.T) {
+	triggers := []map[string]interface{}{
+		{"type": "RISK_ASSESSMENT", "enabled": false},
+		{"type": "too_many_login_failures", "enabled": true},
+	}
+
+	trigger, ok := findTrigger(triggers, "TOO_MANY_LOGIN_FAILURES")
+	if !ok {
+		t.Fatal("findTrigger did not find normalized trigger")
+	}
+	if enabled, _ := trigger["enabled"].(bool); !enabled {
+		t.Fatalf("enabled = false, want true")
+	}
+}
+
+func TestFindTriggerReturnsFalseForMissingType(t *testing.T) {
+	if _, ok := findTrigger([]map[string]interface{}{{"type": "RISK_ASSESSMENT"}}, "TOO_MANY_LOGIN_FAILURES"); ok {
+		t.Fatal("findTrigger returned true for missing trigger")
+	}
+}
+
+func TestStringValuesSkipsNullUnknownAndPreservesValues(t *testing.T) {
+	setValue, diags := types.SetValue(
+		types.StringType,
+		[]attr.Value{
+			types.StringValue("notifier-1"),
+			types.StringNull(),
+			types.StringUnknown(),
+			types.StringValue("notifier-2"),
+		},
+	)
+	if diags.HasError() {
+		t.Fatalf("build set value: %v", diags)
+	}
+
+	values := stringValues(setValue)
+
+	if len(values) != 2 || values[0] != "notifier-1" || values[1] != "notifier-2" {
+		t.Fatalf("values = %#v, want notifier-1,notifier-2", values)
+	}
+}
+
+func TestStringValuesReturnsEmptyForNullOrUnknownSet(t *testing.T) {
+	for _, setValue := range []types.Set{
+		types.SetNull(types.StringType),
+		types.SetUnknown(types.StringType),
+	} {
+		if values := stringValues(setValue); len(values) != 0 {
+			t.Fatalf("values = %#v, want empty", values)
+		}
+	}
+}
+
+func TestReadIntoModelMapsAlertTriggerResponse(t *testing.T) {
+	model := AlertTriggerModel{
+		DomainID: types.StringValue("domain-id"),
+		Type:     types.StringValue("risk_assessment"),
+	}
+
+	readIntoModel(&model, map[string]interface{}{
+		"type":           "too_many_login_failures",
+		"enabled":        true,
+		"alertNotifiers": []interface{}{"notifier-1", "notifier-2"},
+	})
+
+	if model.ID.ValueString() != "domain-id/TOO_MANY_LOGIN_FAILURES" {
+		t.Fatalf("id = %q, want domain-id/TOO_MANY_LOGIN_FAILURES", model.ID.ValueString())
+	}
+	if model.Type.ValueString() != "TOO_MANY_LOGIN_FAILURES" {
+		t.Fatalf("type = %q, want TOO_MANY_LOGIN_FAILURES", model.Type.ValueString())
+	}
+	if !model.Enabled.ValueBool() {
+		t.Fatal("enabled = false, want true")
+	}
+	values := stringValues(model.AlertNotifierIDs)
+	if len(values) != 2 || values[0] != "notifier-1" || values[1] != "notifier-2" {
+		t.Fatalf("alert notifier ids = %#v, want notifier-1,notifier-2", values)
+	}
+}
+
+func TestReadIntoModelSetsEmptyNotifierSetWhenAPIOmitsValues(t *testing.T) {
+	model := AlertTriggerModel{
+		DomainID: types.StringValue("domain-id"),
+		Type:     types.StringValue("risk_assessment"),
+	}
+
+	readIntoModel(&model, map[string]interface{}{
+		"enabled": false,
+	})
+
+	if model.ID.ValueString() != "domain-id/RISK_ASSESSMENT" {
+		t.Fatalf("id = %q, want domain-id/RISK_ASSESSMENT", model.ID.ValueString())
+	}
+	if model.Type.ValueString() != "RISK_ASSESSMENT" {
+		t.Fatalf("type = %q, want RISK_ASSESSMENT", model.Type.ValueString())
+	}
+	if !model.AlertNotifierIDs.IsNull() && len(model.AlertNotifierIDs.Elements()) != 0 {
+		t.Fatalf("alert notifier ids = %#v, want empty set", model.AlertNotifierIDs)
+	}
+}
